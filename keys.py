6135e718727fef3b9d8edb848688c42548688221
Jessy Campos <campos@esetlabs.com>
2016-07-26 15:06:38 -0400

    Add keys.py example

---
+++ keys.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+misp_url = 'MISP URL'
+misp_key = 'MISP KEY' # The MISP auth key can be found on the MISP web interface under the automation section
+pymisp_path = "MISP local PATH"
+misp_verifycert = True
